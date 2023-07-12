8977889198c39027d3a4d6f02318f27eb337491f
Nivarthana1994 <niva2sandeepani@gmail.com>
2023-07-12 12:52:16 +0530

    google auth modified

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const passport = require('passport');
-const googleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('keys');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-passport.use(new googleStrategy({
+passport.use(new GoogleStrategy({
     clientID : keys.googleClientID,
     clientSecret: keys.googleSecretKey,
-    callbackURL:'auth/google/callback'
+    callbackURL:'/auth/google/callback'
 },(accessToken) =>{
     console.log(accessToken);
 }));
@@ -20,6 +20,13 @@ app.get('/auth/google',
             scope: ['profile','email']
         }
     ));
-const PORT = process.env.PORT || 5000
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+const PORT = process.env.PORT || 5002
 
 app.listen(PORT);
+
+/* https://accounts.google.com/signin/oauth/error/
+v2?authError=ChVyZWRpcmVjdF91cmlfbWlzbWF0Y2gSsAEKWW91IGNhbid0IHNpZ24gaW4gdG8gdGhpcyBhcHAgYmVjYXVzZSBpdCBkb2Vzbid0IGNvbXBseSB3aXRoIEdvb2dsZSdzIE9BdXRoIDIuMCBwb2xpY3kuCgpJZiB5b3UncmUgdGhlIGFwcCBkZXZlbG9wZXIsIHJlZ2lzdGVyIHRoZSByZWRpcmVjdCBVUkkgaW4gdGhlIEdvb2dsZSBDbG91ZCBDb25zb2xlLgogIBptaHR0cHM6Ly9kZXZlbG9wZXJzLmdvb2dsZS5jb20vaWRlbnRpdHkvcHJvdG9jb2xzL29hdXRoMi93ZWItc2VydmVyI2F1dGhvcml6YXRpb24tZXJyb3JzLXJlZGlyZWN0LXVyaS1taXNtYXRjaCCQAyo_CgxyZWRpcmVjdF91cmkSL2h0dHA6Ly9sb2NhbGhvc3Q6NTAwMi9hdXRoL2F1dGgvZ29vZ2xlL2NhbGxiYWNrMqQCCAESsAEKWW91IGNhbid0IHNpZ24gaW4gdG8gdGhpcyBhcHAgYmVjYXVzZSBpdCBkb2Vzbid0IGNvbXBseSB3aXRoIEdvb2dsZSdzIE9BdXRoIDIuMCBwb2xpY3kuCgpJZiB5b3UncmUgdGhlIGFwcCBkZXZlbG9wZXIsIHJlZ2lzdGVyIHRoZSByZWRpcmVjdCBVUkkgaW4gdGhlIEdvb2dsZSBDbG91ZCBDb25zb2xlLgogIBptaHR0cHM6Ly9kZXZlbG9wZXJzLmdvb2dsZS5jb20vaWRlbnRpdHkvcHJvdG9jb2xzL29hdXRoMi93ZWItc2VydmVyI2F1dGhvcml6YXRpb24tZXJyb3JzLXJlZGlyZWN0LXVyaS1taXNtYXRjaA%3D%3D&
+
+client_id=575554876724-fi4dv7t9fugi23147qbsv7h9htnmjtba.apps.googleusercontent.com */
